c913d43453440692dd5d9e6c2e12e14540e3b1ca
George Chaniotakis <haniotinho@gmail.com>
2019-02-12 18:44:47 +0200

    location and vehicle class created

---
+++ DesignPatternsDemo/Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsDemo
+{
+    public class Location
+    {
+        public double Latitude { get; }
+
+        public double Longtitude { get; }
+
+        public Location(double latitude, double longtitude)
+        {
+            Latitude = latitude;
+            Longtitude = longtitude;
+        }
+    }
+}
---
+++ DesignPatternsDemo/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsDemo
+{
+    public class Vehicle
+    {
+        public string Id { get; }
+
+        public Location Location { get; }
+
+        public Vehicle (string id)
+        {
+            Id = id;
+        }
+    }
+}
